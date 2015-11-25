00ca5a39f415fd633a3ab526238da5b1d9737a17
amtika <amtika27@gmail.com>
2015-11-25 12:04:22 +0700

    Update head.h

--- head.h
+++ head.h
@@ -4,6 +4,13 @@
 #include <conio.h>
 #include <stdlib.h>
 using namespace std;
+#define first(L) L.first
+#define last(L) L.last
+#define next(P) P->next
+#define prev(P) P->prev
+#define info_akun(P) P->info
+#define info_status(P) P->info
+
 
 struct infotype_akun {
     string id_akun,nama,tgl_lahir,username,password;
@@ -29,6 +36,7 @@ struct list_status {
 struct elmakun {
     infotype_akun info_akun;
     address_akun next;
+    address_akun prev;
     list_status lstatus;
 };
 
@@ -36,18 +44,21 @@ struct list_akun {
     address_akun first;
 };
 
-void addAkun (list_akun &);
-void findAkun (list_akun );
-void editAkun (list_akun &);
-void addStatus (list_akun &, list_status &);
-void findStatus (list_akun , list_status);
-address deleteStatus (list_akun &, list_status);
-void viewStatus (list_akun, list_status);
-address deleteAkun (list_akun &);
-int count_akun (list_akun);
-int count_status (list_akun,list_status);
-void sort_akun (list_akun &);
-void sort_status (list_status &);
-void insertFirstStatus (list_status &);
-void insertFirst
-#endif //HEAD_H_INCLUDED
+void addAkun (list_akun &A);
+address_akun alokasi_akun (infotype_akun x);
+void findAkun (list_akun A);
+void editAkun (list_akun &A);
+void addStatus (list_akun &A, list_status &S);
+void findStatus (list_akun A, list_status L);
+address_status deleteStatus (list_akun &A, list_status S);
+void viewStatus (list_akun A, list_status S);
+address_akun deleteAkun (list_akun &A);
+int count_akun (list_akun A);
+int count_status (list_akun A,list_status S);
+void sort_akun (list_akun &A);
+void sort_status (list_status &S);
+void insertFirstAkun (list_akun &A, address_akun P);
+void insertFirstStatus (list_status &S);
+void deletefirst_akun(list_akun &A,address_akun P);
+void deleteLast_status(list_status &S,address_status *P);
+#endif // HEAD_H_INCLUDED
