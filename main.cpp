795c456bc9b5a01cd9c72ad4595b881f755b0610
yulizar <erwinyulizar14@gmail.com>
2015-11-28 12:23:37 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-// main program
 #include <iostream>
 #include "head.h"
 using namespace std;
@@ -11,16 +10,17 @@ int main()
     bool home = true;
 
     createListAkun(Akun);
-    createListStatus(Status);
     /*std::vector<int> x(10);
     std::iota(std::begin(x), std::end(x), 0); //0 is the starting number
-    *///-> Buat nentuin range
+    *////-> Buat nentuin range
     while (home){
         cout<<"============== WELCOME TO BOOKFACE ==============="<<endl<<endl
             <<"MENU :"<<endl
             <<"1. Make Account "<<endl
             <<"2. Log In "<<endl
-            <<"3. Exit"<<endl<<endl
+            <<"3. view Akun "<<endl
+            <<"4. Count Akun "<<endl
+            <<"5. Exit"<<endl<<endl
             <<"Pilihan Anda : ";cin>>pilih_menu;
         system("cls");
             switch (pilih_menu){
@@ -33,6 +33,16 @@ int main()
                 system("cls");
                 break;
             case 3:
+                viewAkun(Akun);
+                getch();
+                system("cls");
+                break;
+            case 4:
+                cout<<"Akun yang terdaftar ada :"<<count_akun(Akun)<<" akun";
+                getch();
+                system("cls");
+                break;
+            case 5:
                 home = false;
                 break;
             default: break;
